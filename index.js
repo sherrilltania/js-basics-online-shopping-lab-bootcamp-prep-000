f33ac286291191ee82b745a36bece4f164300cf3
Tatiana Sherrill <anilorac_sol@yahoo.com>
2018-07-31 21:27:02 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ function setCart(c) {
 }
 
 function addToCart(item){
-var price = Math.floor(Math.random()* 100)+1;
+var price = Math.floor(Math.random()* 100);
 
 cart.push({itemName: item, itemPrice: price});
 console.log("${item} has been added to your cart.");
