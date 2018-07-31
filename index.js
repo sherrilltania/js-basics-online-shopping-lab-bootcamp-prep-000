d55a92f921968343e110e9d20f0d9baff8699719
Tatiana Sherrill <anilorac_sol@yahoo.com>
2018-07-31 20:20:33 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -30,7 +30,7 @@ return cart
      console.log("In your cart, you have" + newCart.join("and ") + ".")
    }
    else if(cart.length > 2){
-     console.log("In your cart, you have" + newCart.slice(0, -1).join(", ") + ", and " + newCart.slice(-1)
+     console.log("In your cart, you have" + newCart.slice(0, -1).join(", ") + ", and " + newCart.slice(-1))
    }
    else if (cart.length === 0){
      console.log("Your shopping cart is empty.")
